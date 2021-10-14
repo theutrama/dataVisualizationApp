26d30f5342cbcefc3622bf95af0972b98bf7124e
theutrama <jonas.koch@tu-dortmund.de>
2021-10-14 11:46:28 +0200

    Initial Commit

---
+++ dataVisualizationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dataVisualizationApp
+//
+//  Created by Jonas Koch on 14.10.21.
+//  Copyright © 2021 Jonas Koch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
